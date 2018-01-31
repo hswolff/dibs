2da754f800ee79e6dd5ee046255a34c24b8e281d
Harry Wolff <hswolff@gmail.com>
2018-01-31 18:49:39 -0500

    feat: Add support for emotion for css

--- client/nwb.config.js
+++ client/nwb.config.js
@@ -1,3 +1,15 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
-  type: 'react-app'
-}
+  type: 'react-app',
+  babel: {
+    plugins: [
+      [
+        'emotion',
+        isProduction
+          ? { hoist: true, extractStatic: true }
+          : { sourceMap: true, autoLabel: true },
+      ],
+    ],
+  },
+};
--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,10 @@
-import './App.css';
-
 import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-heading App-flex">
-          <h2>
-            Welcome to <span className="App-react">React</span>
-          </h2>
-        </div>
-        <div className="App-instructions App-flex">
-          <p>
-            Edit <code>src/App.js</code> and save to hot reload your changes.
-          </p>
-        </div>
+      <div>
+        <h1 css={{ color: 'red', fontSize: '33px' }}>Hello world</h1>
       </div>
     );
   }
