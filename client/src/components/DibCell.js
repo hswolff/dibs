94f59a87e78fb6032f08deb5c58696c05496b00c
Harry Wolff <hswolff@gmail.com>
2018-06-06 15:43:25 -0400

    Let's call dibs: Client support

--- client/src/components/DibCell.js
+++ client/src/components/DibCell.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 import './DibCell.css';
 
 export default class DibCell extends Component {
+  claimDib = async () => {
+    this.setState({ error: null });
+
+    try {
+      await api.claimDib({
+        id: this.props._id,
+        user: this.props.username,
+      });
+    } catch (error) {
+      console.error('Error claiming dib', error);
+    }
+  };
+
   render() {
-    const { title, creator } = this.props;
+    const { title, creator, claimed } = this.props;
+
+    const isClaimed = claimed && claimed.user != null;
 
     return (
       <div className="dib-container">
@@ -13,7 +29,11 @@ export default class DibCell extends Component {
           </div>
           <div className="dib-title">{title}</div>
         </div>
-        <div className="dib-right" />
+        <div className="dib-right">
+          <button className="dib-claim-button" onClick={this.claimDib}>
+            {isClaimed ? 'Claimed!' : 'Dibs?'}
+          </button>
+        </div>
       </div>
     );
   }
